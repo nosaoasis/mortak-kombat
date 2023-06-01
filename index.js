a1d1523318ab63195c6433b15a3f3c993c350794
Nosa Agho <nosaoasis@gmail.com>
2023-06-01 20:50:07 +0100

    Players and Gravity
    Used the canvas to create two players and made sure the players are at the bottom of the screen using 'gravity'.

--- index.js
+++ index.js
@@ -1,28 +1,66 @@
-const canvas = document.querySelector("canvas")
+const canvas = document.querySelector("canvas");
 
-const c = canvas.getContext("2d") // canvas context
+const c = canvas.getContext("2d"); // canvas context
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
 class Sprite {
-  constructor(position) {
-    this.position = position
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
   }
 
   draw() {
-    c.fillStyle = "blue"
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillStyle = "blue";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else this.velocity.y += gravity;
   }
 }
 
+// create player 1 => player
 const player = new Sprite({
-  x:0,
-  y:0
-})
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
 
-player.draw()
+// create player 2 => enemy
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
 
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "yellow";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
 
+animate();
