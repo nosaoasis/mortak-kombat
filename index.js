4a7e93e1c69d2067564ba600ec35cd74e6714761
Nosa Agho <nosaoasis@gmail.com>
2023-06-02 06:25:05 +0100

    Player Movement
    Added features for moving the players when the defined keys are pressed.

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ class Sprite {
   update() {
     this.draw();
 
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -55,12 +56,54 @@ const enemy = new Sprite({
   },
 });
 
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+let lastKey
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "yellow";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0
+
+  if (keys.a.pressed && lastKey === "a") {
+    player.velocity.x = -1
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.velocity.x = 1
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = true
+      lastKey = "d"
+      break;
+      case "a":
+        keys.a.pressed = true
+        lastKey = "a"
+      break;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = false
+      break;
+    case "a":
+      keys.a.pressed = false
+      break;
+  }
+});
